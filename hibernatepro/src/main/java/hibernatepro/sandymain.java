2a2f241ea13b546b1511486a183dabd783e8a0db
PRAVEEN <PRAVEEN@praveen>
2022-11-14 15:19:59 +0530

    14.11.22

---
+++ hibernatepro/src/main/java/hibernatepro/sandymain.java
@@ -0,0 +1,11 @@
+package hibernatepro;
+
+public class sandymain {
+
+	public static void main(String[] args) {
+ 
+		int l= 700;
+		
+	}
+
+}
---
+++ hibernatepro/src/main/java/hibernatepro/sandypersist.java
@@ -0,0 +1,37 @@
+package hibernatepro;
+
+public class sandypersist //table name
+{
+	int id;
+	String name;
+	int age;
+	int num;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public sandypersist() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
